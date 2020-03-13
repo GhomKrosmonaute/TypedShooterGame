2434fafe9865b8bcee87aba5da25812416472938
Camille Abella <camille.abella@hotmail.fr>
2020-03-13 16:38:12 +0100

    remove old d.ts files

--- dist/Shooter/Bonus/Haki.d.ts
+++
@@ -1,8 +0,0 @@
-import Bonus from '../Bonus';
-import { Passive } from '../../interfaces';
-import p5 from 'p5';
-export default class Shield extends Bonus implements Passive {
-    level: number;
-    shape(p: p5, x: number, y: number, w: number, h: number): void;
-    applyEffect(): void;
-}
