ac1dc0fab6ef11f11d36d4f2e324a1ca582f3bdc
CamilleAbella <camille.abella@hotmail.fr>
2020-03-18 18:42:12 +0100

    clean dist folder

--- dist/Shooter/Rate.d.ts
+++
@@ -1,7 +0,0 @@
-export default class Rate {
-    interval: number;
-    private triggerTime;
-    constructor(interval: number);
-    canTrigger(trigger?: boolean): boolean;
-    trigger(): void;
-}
--- dist/Shooter/Scenes/Party.d.ts
+++
@@ -1,5 +0,0 @@
-import Scene from '../Scene';
-export default class PartyScene extends Scene {
-    draw(): void;
-    step(): Promise<void>;
-}
